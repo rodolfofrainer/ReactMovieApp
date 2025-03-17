1ea05c1243455ca04e5d9d6a232ed6ae7e6f4f49
rodolfo frainer <rodolfofrainer@gmail.com>
2025-03-17 10:32:50 +0100

    create top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div className="app-container">
+      <header>
+        <div className="logo">
+          <button>Movie App</button>
+        </div>
+        <div className="links">
+          <button className="home">Home</button>
+          <button className="favorites">Favorites</button>
+        </div>
+      </header>
+      <div className="search-bar-form">
+        <form method="get">
+          <input type="text" name="search-bar" id="searchbar" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
