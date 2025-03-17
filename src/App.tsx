6a8137ac258fb6a7d34f2896a1ecd3e8569be9a5
rodolfo frainer <rodolfofrainer@gmail.com>
2025-03-17 15:20:43 +0100

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Header from "./components/Header";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app-body">
+    <main className="app-body">
       <Header />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/favorites"
+          element={
+            <div className="fav-div">
+              <Favorites />
+            </div>
+          }
+        />
+      </Routes>
+    </main>
   );
 }
 
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css";
+
+export default function Favorites() {
+  return (
+    <div className="favorites">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+  );
+}
